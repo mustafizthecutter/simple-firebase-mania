96d93fb7b4f7a82b2291b690c85748eaec68d599
Mustafizur Rahman <mustafizur232@gmail.com>
2023-12-23 05:14:22 +0600

    sign in and sign out toggled

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,23 +1,45 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
+import { useState } from "react";
 
 
 const Login = () => {
+    const [user, setUser] = useState(null);
     const auth = getAuth(app);
-    const provider = new GoogleAuthProvider()
+    const provider = new GoogleAuthProvider();
     const handleGoogleSignIn = () => {
         signInWithPopup(auth, provider)
             .then(result => {
-                const user = result.user;
-                console.log(user);
+                const loggedInUser = result.user;
+                console.log(loggedInUser);
+                setUser(loggedInUser);
             })
             .catch(error => {
                 console.log(error.message);
             })
     }
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(result => {
+                console.log(result);
+                setUser(null);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
     return (
         <div>
-            <button onClick={handleGoogleSignIn}>Go to Login</button>
+            {
+                user ? <button onClick={handleSignOut}>Sign Out</button> : <button onClick={handleGoogleSignIn}>Go to Login</button>
+            }
+
+
+            {user && <div>
+                <h2>Name: {user.displayName}</h2>
+                <p>{user.email}</p>
+                <img src={user.photoURL} alt="" />
+            </div>}
         </div>
     );
 };
